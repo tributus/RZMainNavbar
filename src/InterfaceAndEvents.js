4f3b75f0fbeddb490ddd4f70616f4f3a126c2c35
anderson.santos <anderson.santos@pcinformatica.com.br>
2016-05-11 17:48:53 -0300

    commit inicial de criação do projeto

---
+++ src/InterfaceAndEvents.js
@@ -0,0 +1,7 @@
+/**
+ * Created by anderson.santos on 11/05/2016.
+ */
+rz.widgets.RZMainNavbarWidgetHelper = {
+    MainNavbarWidgetInterface : [],
+    MainNavbarWidgetEventHandlers : []
+};
---
+++ src/MainNavbarWidget.js
@@ -0,0 +1,13 @@
+/**
+ * Created by anderson.santos on 11/05/2016.
+ */
+rz.widgets.FormWidget = ruteZangada.widget("rzMainNavbar",rz.widgets.RZMainNavbarWidgetHelper.MainNavbarWidgetInterface,rz.widgets.RZMainNavbarWidgetHelper.MainNavbarWidgetEventHandlers,function () {
+    var $this = this;
+    this.initialize = function (params, initialized) {
+
+    };
+
+    this.render = function (target, params) {
+        
+    };
+});
